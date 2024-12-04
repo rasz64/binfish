62c052cc5ade497d7ec7650e8ea2151826f3de38
rasz64 <157414990+rasz64@users.noreply.github.com>
2024-12-03 22:13:17 -0800

    Update index.js

--- Shellminal/unixium/index.js
+++ Shellminal/unixium/index.js
@@ -1,9 +1,9 @@
 function toggleSection(id) {
     const section = document.getElementById(id);
     if (section.style.display === '' || section.style.display === 'none') {
-        section.style.display = 'block'; // Visa sektionen
+        section.style.display = 'block';
     } else {
-        section.style.display = 'none'; // Dölj sektionen
+        section.style.display = 'none';
     }
 }
 
